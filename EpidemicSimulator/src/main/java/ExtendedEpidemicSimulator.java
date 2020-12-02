081b0dd69af4084d124a08ddb9c0b8c5a08112cc
Mark <m_he@uncg.edu>
2020-12-02 15:07:58 -0500

    Updated Part 1

--- EpidemicSimulator/src/main/java/ExtendedEpidemicSimulator.java
+++ EpidemicSimulator/src/main/java/ExtendedEpidemicSimulator.java
@@ -1,29 +1,72 @@
 import java.util.Random;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.stage.Stage;
+
 /**
  * @author Ylewi Adrong & Mark He
  * @class: CSC 407A - Network Analysis
  * @date: December 3, 2020
  * 
- *        To-do: 
- *        [X]: 1 [X]: 2 [X]: 3 [X]: 4 [ ]: 5 [X]: 6 [X]: 7 [ ]: 8 
- *      ------ ------------------------------------------------------
- *        [ ]: a [ ]: b [ ]: c [ ]: d [ ]: e [ ]: f
+ *     To-do: 
+ *      [X]: 1 [X]: 2 [X]: 3 [X]: 4 [ ]: 5 [X]: 6 [X]: 7 [ ]: 8 
+ *    ------------------------------------------------------------ 
+ *      [ ]: a [ ]: b []: c [ ]: d [ ]: e [ ]: f
  * 
  */
-public class ExtendedEpidemicSimulator {
+public class ExtendedEpidemicSimulator extends Application{
 
+    @Override 
+    public void start(Stage stage) {
+        stage.setTitle("Rate of Infection Graph");
+
+        //defining x, y
+        final NumberAxis xAxis = new NumberAxis();
+        final NumberAxis yAxis = new NumberAxis();
+        xAxis.setLabel("Rounds");
+        yAxis.setLabel("Total Infected");
+
+        //creating the chart
+        final LineChart<Number,Number> lineChart = 
+                new LineChart<Number,Number>(xAxis,yAxis);
+                
+        lineChart.setTitle("Rate of Infection (T1, T2)");
+        //defining a series
+        XYChart.Series series = new XYChart.Series();
+        // series.setName("Infected");
+        
+        //populating the series with data
+        for (int i=1;i<T;i++){
+            for (int j=0;j<infected.length;j++){
+                if (infected[j]==1){
+                    series.getData().add(new XYChart.Data(i, j));
+                }
+            }
+        }
+        
+        Scene scene  = new Scene(lineChart,800,600);
+        lineChart.getData().add(series);
+       
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    //infection status
     public static final int HEALTHY = 0, INFECTED = 1, EXPOSED = 2, RECOVERED = 3, IMMUNE = 4;;
     public static final String[] statusString = { "Healthy", "Infected", "Exposed", "Recovered" };
 
     // Total population N = 1000;
-    public static int N = 1000;
+    public static int N = 1001;
     public static int[] infected = new int[N];
 
-    public static int T = 100;
+    public static int T = 101;
     public static int total_infected;
-    public static double a = 1;
-    public static double b = 1;
+    public static double a = 0.25;
+    public static double b = 0.25;
 
     /**
      * Contact Rate True if 0 <= a <= 1 False otherwise
@@ -52,8 +95,9 @@ public class ExtendedEpidemicSimulator {
         int recoveredCount = 0;
         int recoveredTotal = 0;
         int immuneCount = 0;
+        double R0;
 
-        for (int i = 0; i < T; i++) {
+        for (int i = 1; i < T; i++) {
             System.out.println("\n|--------------| ROUND {" + i + "} |--------------|");
             infectedCount = 0;
             exposedCount = 0;
@@ -61,7 +105,7 @@ public class ExtendedEpidemicSimulator {
             immuneCount++;
 
             System.out.println(immuneCount);
-            for (int j = 0; j < infected.length; j++) {
+            for (int j = 1; j < infected.length; j++) {
                 if (infected[j] == HEALTHY || infected[j] == EXPOSED) {
                     if (infected[j] == EXPOSED || isExposed()) {
                         infected[j] = EXPOSED;
@@ -75,13 +119,14 @@ public class ExtendedEpidemicSimulator {
                         }
                     }
                 }
-                for (int k = 0; k <= j; k++) {
+                for (int k = 1; k <= j; k++) {
                     if (infected[k] == INFECTED && immuneCount % 5 == 0) {
                         infected[k] = RECOVERED;
                         recoveredCount++;
                         recoveredTotal++;
                         infectedCount--;
                         totalInfected--;
+
                     } else if (infected[k] == RECOVERED && immuneCount % 20 == 0) {
                         infected[k] = HEALTHY;
                         recoveredCount--;
@@ -100,6 +145,19 @@ public class ExtendedEpidemicSimulator {
             System.out.println("There have been " + recoveredCount + " recovered patients in round " + i);
             System.out.println("Total recovered: " + recoveredTotal);
             System.out.println("Total infected: " + totalInfected);
+
+            /**
+             * Formula for R0:
+             * 
+             *      R0 = (infection/contact) * (contact/time) * (time/infection)
+             
+
+            if (infectedCount > 0 && exposedCount > 0) {
+                System.out.println("Infected Count = "  + infectedCount + "\nExposed Count = " + exposedCount +"\nRounds = " + i + "\n");
+                R0 =  ((double) infectedCount / (double) exposedCount) * ((double) exposedCount / (double) i) * ((double) i / (double) infectedCount);
+                System.out.println("On average of " + R0 + " newly infected per " + exposedCount + " exposed");
+            }
+            */
         }
     }
 
@@ -109,5 +167,6 @@ public class ExtendedEpidemicSimulator {
         // epidemic.totalInfected(infected);
 
         totalInfected(infected);
+        launch(args);
     }
 }
